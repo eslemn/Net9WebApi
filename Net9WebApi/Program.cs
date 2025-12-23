41136e28000d8f557c0c74bf654ce62be449e113
Eslemnur Ertaş <eslemnur.ertas@icloud.com>
2025-12-23 15:29:55 +0300

    feat: initial project setup with swagger and gitignore

---
+++ Net9WebApi/Program.cs
@@ -0,0 +1,21 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Swagger services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Swagger middleware
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+// Test endpoint
+app.MapGet("/", () => "API is running");
+
+app.Run();
