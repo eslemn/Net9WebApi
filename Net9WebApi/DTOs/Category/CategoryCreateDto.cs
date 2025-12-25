d1183ec8d5ceb889652ade9d041cfee5e77d51dc
Eslemnur Ertaş <eslemnur.ertas@icloud.com>
2025-12-25 13:23:19 +0300

    feat: add category dtos

---
+++ Net9WebApi/DTOs/Category/CategoryCreateDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Net9WebApi.DTOs.Category
+{
+    public class CategoryCreateDto
+    {
+        public string Name { get; set; } = null!;
+    }
+}
---
+++ Net9WebApi/DTOs/Category/CategoryUpdateDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Net9WebApi.DTOs.Category
+{
+    public class CategoryUpdateDto
+    {
+        public string Name { get; set; } = null!;
+    }
+}
